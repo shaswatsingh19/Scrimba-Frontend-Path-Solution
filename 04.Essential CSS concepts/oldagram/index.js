453465ac9d7c74db88f35cff07cb0a186ae57c35
shaswat singh <shaswatsingh.ose@gmail.com>
2022-08-07 18:58:58 +0530

    added infinite scroll, like and dislike feature

--- 04.Essential CSS concepts/oldagram/index.js
+++ 04.Essential CSS concepts/oldagram/index.js
@@ -11,10 +11,10 @@ const postsArray = [
     {
         name: "Gustave Courbet",
         username: "gus1819",
-        location: "Ornans, France",
+        location: "New York, America",
         avatar: "images/avatar-courbet.jpg",
         post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
+        comment: "Life is Great",
         likes: 4
     },
         {
@@ -24,8 +24,36 @@ const postsArray = [
         avatar: "images/avatar-ducreux.jpg",
         post: "images/post-ducreux.jpg",
         comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
+        likes: 32
+    },
+    {
+        name: "Mona Lisa",
+        username: "papakapara125",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "Drinking few Shorts",
+        likes: 121,
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed ",
+        likes: 125
+    },
+        {
+        name: "Munna Tripathi",
+        username: "jd1735",
+        location: "Varanasi, India",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "I am having fun",
+        likes: 105
+        }
+    
 ]
 
 
@@ -33,9 +61,7 @@ let id  = 0
 const main = document.querySelector('main')
 let likeBtnPressed = false
 
-// if(document.body.scrollBy(500)){
-//     console.log('sd')
-// }
+
 
 window.onscroll = (e)=>{
     if(window.scrollY+1000 > document.body.scrollHeight){
@@ -82,47 +108,7 @@ window.onscroll = (e)=>{
         }
 }
 
-// postsArray.forEach(p => {
-    
-//     const div  = document.createElement('div')
-    
-//     div.classList.add('user')
-//     div.setAttribute('id',`user-${id}`)
-//     id+=1
 
-//     div.innerHTML =`<section class="user-info">
-//     <img class="profile-pic" src = ${p.avatar} alt="user {id} images">
-//     <div class="name-location">
-//         <h4 class='name'>${p.name}</h4>
-//         <p class='location'>${p.location}</p>
-//     </div>
-//     </section>
-
-//     <section class="post-container">
-//         <img class='post' src="${p.post}" alt="${p.name}'s post" ondblclick=updateLikesViaPost(event)>
-//     </section>
-
-//     <section class="post-info-container">
-//         <div class="like-follow-btn">
-//             <img src="images/icon-heart.png" class="like-btn" alt="like button" onclick=updateLikesViaLike(event)>
-//             <img src="images/icon-comment.png" class="comment-btn"alt="comments button">
-//             <img src="images/icon-dm.png" class="dm-btn"alt="dm button">
-//         </div>
-//         <div class="likes-container">
-//             <h4 class="total-likes"><span class='likes'>${p.likes}</span> likes</h4>
-//         </div>
-//         <div class="comments-container">
-//             <div class="comments">
-//                 <h4 class="username">${p.username}</h4>
-//                 <p class="comment">${p.comment}</p>
-//             </div>
-            
-//         </div>
-//     </section>
-//     `
-//     main.append(div)
-    
-// })
 
 function updateLikes(node){
 
