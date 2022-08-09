a0863ddace28de62a6052586296c92e6ba7737f9
shaswat singh <shaswatsingh.ose@gmail.com>
2022-08-09 10:40:11 +0530

    minor updates

--- 04.Essential CSS concepts/oldagram/index.js
+++ 04.Essential CSS concepts/oldagram/index.js
@@ -1,61 +1,3 @@
-const postsArray = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21,
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "New York, America",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "Life is Great",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 32
-    },
-    {
-        name: "Mona Lisa",
-        username: "papakapara125",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "Drinking few Shorts",
-        likes: 121,
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed ",
-        likes: 125
-    },
-        {
-        name: "Munna Tripathi",
-        username: "jd1735",
-        location: "Varanasi, India",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "I am having fun",
-        likes: 105
-        }
-    
-]
-
 
 let id  = 0
 const main = document.querySelector('main')
@@ -63,8 +5,8 @@ let likeBtnPressed = false
 
 
 
-window.onscroll = (e)=>{
-    if(window.scrollY+1000 > document.body.scrollHeight){
+addEventListener('wheel',(e)=>{
+    if(window.scrollY+700 > document.body.scrollHeight){
         let randomIndex = Math.floor(Math.random() *postsArray.length)
         console.log(randomIndex)
 
@@ -106,7 +48,7 @@ window.onscroll = (e)=>{
         `
         main.append(div)
         }
-}
+})
 
 
 
---
+++ 04.Essential CSS concepts/oldagram/posts.js
@@ -0,0 +1,57 @@
+const postsArray = [
+    {
+        name: "Vincent van Gogh",
+        username: "vincey1853",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "just took a few mushrooms lol",
+        likes: 21,
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "New York, America",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "Life is Great",
+        likes: 4
+    },
+        {
+        name: "Joseph Ducreux",
+        username: "jd1735",
+        location: "Paris, France",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+        likes: 32
+    },
+    {
+        name: "Mona Lisa",
+        username: "papakapara125",
+        location: "Zundert, Netherlands",
+        avatar: "images/avatar-vangogh.jpg",
+        post: "images/post-vangogh.jpg",
+        comment: "Drinking few Shorts",
+        likes: 121,
+    },
+    {
+        name: "Gustave Courbet",
+        username: "gus1819",
+        location: "Ornans, France",
+        avatar: "images/avatar-courbet.jpg",
+        post: "images/post-courbet.jpg",
+        comment: "i'm feelin a bit stressed ",
+        likes: 125
+    },
+        {
+        name: "Munna Tripathi",
+        username: "jd1735",
+        location: "Varanasi, India",
+        avatar: "images/avatar-ducreux.jpg",
+        post: "images/post-ducreux.jpg",
+        comment: "I am having fun",
+        likes: 105
+        }
+    
+]
