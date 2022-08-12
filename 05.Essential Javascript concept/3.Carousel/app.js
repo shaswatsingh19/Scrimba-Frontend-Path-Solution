08f99d43d217ead28c6b9994309a60f6f6d9b35f
shaswat singh <shaswatsingh.ose@gmail.com>
2022-08-12 23:26:09 +0530

    completed the carousel

---
+++ 05.Essential Javascript concept/3.Carousel/app.js
@@ -0,0 +1,29 @@
+let carousel = document.querySelectorAll('.carousel-item')
+let button = document.querySelectorAll('button')
+let currEle = 0
+
+
+function changeImage(){
+    carousel[currEle].classList.toggle('carousel-item-visible')
+}
+
+button.forEach(btn => {
+    btn.addEventListener('click', e=>{
+
+        changeImage()
+        if(e.target.id === 'carousel-button-next'){
+            currEle =   (currEle+1)%carousel.length
+        }else{
+            currEle =   (currEle+carousel.length-1)%carousel.length
+        }
+        changeImage()
+    })
+})
+
+
+
+setInterval(() => {
+    changeImage()
+    currEle =   (currEle+1)%carousel.length
+    changeImage()
+},5000)
