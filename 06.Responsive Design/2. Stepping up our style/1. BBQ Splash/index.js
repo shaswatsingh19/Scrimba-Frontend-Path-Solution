d2b04ebf9d2506408eb419fc3b589477f1b82ea3
shaswat singh <shaswatsingh.ose@gmail.com>
2022-08-21 21:26:45 +0530

    completed the bbq page

---
+++ 06.Responsive Design/2. Stepping up our style/1. BBQ Splash/index.js
@@ -0,0 +1,30 @@
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+
+
+
+nameInput.addEventListener('input',e=>{
+    // console.log(typeof e.target.value)
+
+    let text = e.target.value
+    const reg = /[0-9]/g
+
+    if(text.match(reg)){
+        nameInput.classList.add('shake')
+    }else{
+        nameInput.classList.remove('shake')
+    }
+})
+
+emailInput.addEventListener('input',e=>{
+    
+    let text = e.target.value
+    
+    let isValid = e.target.validity.valid
+
+    if(isValid && text.length > 9 && text.includes('.com')){
+        emailInput.classList.remove('shake')
+    }else{
+        emailInput.classList.add('shake')
+    }
+})
