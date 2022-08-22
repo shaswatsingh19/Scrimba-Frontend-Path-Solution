e07c12caede108986eeec88a3bc23b94ebe7eb5c
shaswat singh <shaswatsingh.ose@gmail.com>
2022-08-22 17:18:24 +0530

    module completed

--- 06.Responsive Design/2. Stepping up our style/1. BBQ Splash/index.js
+++ 06.Responsive Design/2. Stepping up our style/1. BBQ Splash/index.js
@@ -1,16 +1,22 @@
 const nameInput = document.querySelector("#name");
+const nameLabel = document.querySelector('#name-label')
 const emailInput = document.querySelector("#email");
+const emailLabel = document.querySelector('#email-label')
 
 nameInput.addEventListener("input", (e) => {
   // console.log(typeof e.target.value)
 
   let text = e.target.value;
   const reg = /[0-9]/g;
-
+  const p = document.createElement('p')
+  nameLabel.textContent = ''
   if (text.match(reg)) {
-    nameInput.classList.add("shake");
+      nameInput.classList.add("shake");
+      p.textContent ='Digit should not be inside name'
+      nameLabel.appendChild(p)
   } else {
     nameInput.classList.remove("shake");
+    nameLabel.textContent = ''
   }
 });
 
@@ -18,10 +24,18 @@ emailInput.addEventListener("input", (e) => {
   let text = e.target.value;
 
   let isValid = e.target.validity.valid;
-
-  if (isValid && text.length > 9 && text.length > 0 && text.includes(".com")) {
+  const p = document.createElement('p')
+  emailLabel.textContent = ''
+  if (isValid && text.length > 9 && text.includes(".com")) {
     emailInput.classList.remove("shake");
-  } else {
+    emailLabel.textContent = ''
+} else if(text.length>0)    {
     emailInput.classList.add("shake");
+    console.log(text)
+    p.textContent = 'Please enter valid email'
+    emailLabel.appendChild(p)
+  }else{
+    emailInput.classList.remove("shake");
+
   }
 });
