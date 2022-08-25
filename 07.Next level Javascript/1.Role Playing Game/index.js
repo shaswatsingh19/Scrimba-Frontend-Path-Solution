5cc201a9082605f247530be5f0a586555ae24498
shaswat singh <shaswatsingh.ose@gmail.com>
2022-08-25 14:46:21 +0530

    added getDiceRollArray function to generate random number and push those value to diceRoll array

--- 07.Next level Javascript/1.Role Playing Game/index.js
+++ 07.Next level Javascript/1.Role Playing Game/index.js
@@ -1,9 +1,14 @@
+// Generate Random Number from 1 to 10
+let getDiceRollArray = () => { 
+    return Math.ceil(Math.random()*10)
+}
+
 const hero = {
     elementId : "hero",
     name : "Wizard",
     avatar : "images/wizard.png",
     health : 60,
-    diceRoll : [3,1,4],
+    diceRoll : [],
     diceCount :3
 }
 const monster = {
@@ -11,7 +16,7 @@ const monster = {
     name : "Orc",
     avatar : "images/orc.png",
     health : "40",
-    diceRoll : [2,1],
+    diceRoll : [],
     diceCount :2
 }
 function renderCharacter(data) {
@@ -20,12 +25,16 @@ function renderCharacter(data) {
     const {elementId,name,avatar,health,diceRoll , diceCount} = data
 
 
+    // Generating a array of diceCount of random number 
+    for(let i=0; i<diceCount ;i++){
+        diceRoll.push(getDiceRollArray())
+    }
+
+    // making new divs that will go inside dice container 
     let diceHtml =  diceRoll.map(roll => {
         return `<div class="dice">${roll}</div>`
     }).join('</br>')
-    console.log(diceHtml)
-
-
+    
     document.getElementById(elementId).innerHTML =
         `<div class="character-card">
             <h4 class="name"> ${name} </h4>
