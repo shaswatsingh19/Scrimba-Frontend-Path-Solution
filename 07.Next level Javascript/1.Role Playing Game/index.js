f4dca25760fcb98265e384dea33f62b6f8f5b710
shaswat singh <shaswatsingh.ose@gmail.com>
2022-08-25 15:26:13 +0530

    remove diceRoll array and made diceRollArray that do that

--- 07.Next level Javascript/1.Role Playing Game/index.js
+++ 07.Next level Javascript/1.Role Playing Game/index.js
@@ -1,6 +1,10 @@
 // Generate Random Number from 1 to 6
-let getDiceRollArray = () => { 
-    return Math.floor(Math.random()*6 + 1)
+let getDiceRollArray = (diceCount) => { 
+    let diceRoll = []
+    for(let i=0; i<diceCount ;i++){
+        diceRoll.push(Math.floor(Math.random()*6 + 1))
+    }
+    return  diceRoll
 }
 
 const hero = {
@@ -8,7 +12,6 @@ const hero = {
     name : "Wizard",
     avatar : "images/wizard.png",
     health : 60,
-    diceRoll : [],
     diceCount :3
 }
 const monster = {
@@ -16,25 +19,17 @@ const monster = {
     name : "Orc",
     avatar : "images/orc.png",
     health : "40",
-    diceRoll : [],
     diceCount :2
 }
 function renderCharacter(data) {
     
     // we use object destructuring so that we don't need to write object.property everywhere 
-    const {elementId,name,avatar,health,diceRoll , diceCount} = data
+    const {elementId,name,avatar,health, diceCount} = data
 
 
-    // Generating a array of diceCount of random number 
-    for(let i=0; i<diceCount ;i++){
-        diceRoll.push(getDiceRollArray())
-    }
+    // making new divs that will go inside dice container and adding number from getDiceRollArray
+    let diceHtml =  getDiceRollArray(diceCount).map(num => `<div class="dice">${num}</div>`).join('')
 
-    // making new divs that will go inside dice container 
-    let diceHtml =  diceRoll.map(roll => {
-        return `<div class="dice">${roll}</div>`
-    }).join('</br>')
-    
     document.getElementById(elementId).innerHTML =
         `<div class="character-card">
             <h4 class="name"> ${name} </h4>
