8066d84533c3ffc9ba01ad1e446ed119731d0d36
shaswat singh <shaswatsingh.ose@gmail.com>
2022-08-31 16:35:24 +0530

    added basic styling and fetching the data from api then displaying it

---
+++ 09.Working with APIs/02.Blog Space/index.js
@@ -0,0 +1,32 @@
+const blogList = document.getElementById('blog-list')
+
+const url  = 'https://apis.scrimba.com/jsonplaceholder/posts'
+
+fetch(url , {method:"GET"})
+.then(response => response.json())
+.then(data => {
+    
+
+    let top5Posts = data.slice(0,5)
+
+    console.log(top5Posts)
+    let arr =[]
+
+    top5Posts.forEach(data => {
+        const  title = document.createElement('h3')
+        const  p = document.createElement('p')
+        const  div = document.createElement('div')
+
+        title.textContent = data.title
+        p.textContent = data.body.substring(0,1).toUpperCase() + data.body.substring(1,)
+        
+        div.append(title,p)
+        arr.push(div)
+        // document.body.append(title)
+        // document.body.append(p)
+    })
+
+    blogList.append(...arr)
+
+
+})
