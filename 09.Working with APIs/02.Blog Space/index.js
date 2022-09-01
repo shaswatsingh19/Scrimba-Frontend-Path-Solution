dee446eab9c7760a84b0cf912d2a96f78dc4156e
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-01 14:46:46 +0530

    added input field and connected with js

--- 09.Working with APIs/02.Blog Space/index.js
+++ 09.Working with APIs/02.Blog Space/index.js
@@ -1,4 +1,8 @@
 const blogList = document.getElementById('blog-list')
+const submitBtn = document.querySelector('button')
+
+
+
 
 const url  = 'https://apis.scrimba.com/jsonplaceholder/posts'
 
@@ -7,9 +11,8 @@ fetch(url , {method:"GET"})
 .then(data => {
     
 
-    let top5Posts = data.slice(0,45)
+    let top5Posts = data.slice(0,5)
 
-    console.log(top5Posts)
     let arr =[]
 
     top5Posts.forEach(data => {
@@ -30,3 +33,40 @@ fetch(url , {method:"GET"})
 
 
 })
+
+ 
+document.getElementById('new-post').addEventListener('submit',function(e){
+    
+    
+    const postTitle = document.getElementById('post-title')
+    const postBody = document.getElementById('post-body')
+    
+    // Preventing for reloading the form 
+    e.preventDefault()
+
+    const post = {
+        title:postTitle.value,
+        body:postBody.value,
+    }
+    console.log(post)
+
+
+    // Adding to DOM 
+    const  title = document.createElement('h3')
+    const  p = document.createElement('p')
+    const  div = document.createElement('div')
+
+    title.textContent = postTitle.value
+    p.textContent = postBody.value.substring(0,1).toUpperCase() + postBody.value.substring(1,)
+    
+    div.append(title,p)
+    blogList.prepend(div)
+    
+    postTitle.value = ''
+    postBody.value = ''
+
+    
+})
+
+
+
