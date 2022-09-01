3686d8f246ec787381469cb6e4d9a86bf4e182d7
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-01 16:07:02 +0530

    refactor js file

--- 09.Working with APIs/02.Blog Space/index.js
+++ 09.Working with APIs/02.Blog Space/index.js
@@ -1,85 +1,69 @@
-const blogList = document.getElementById('blog-list')
-const submitBtn = document.querySelector('button')
+const blogList = document.getElementById("blog-list");
+const submitBtn = document.querySelector("button");
 
+const url = "https://apis.scrimba.com/jsonplaceholder/posts";
 
+fetch(url, { method: "GET" })
+  .then((response) => response.json())
+  .then((data) => {
+    let top5Posts = data.slice(0, 5);
+    let arr = [];
 
+    top5Posts.forEach((data) => {
+      let newDiv = renderPosts(data.title, data.body);
+      arr.push(newDiv);
+    });
 
-const url  = 'https://apis.scrimba.com/jsonplaceholder/posts'
+    blogList.append(...arr);
+  });
 
-fetch(url , {method:"GET"})
-.then(response => response.json())
-.then(data => {
-    
+document.getElementById("new-post").addEventListener("submit", function (e) {
+  const postTitle = document.getElementById("post-title");
+  const postBody = document.getElementById("post-body");
 
-    let top5Posts = data.slice(0,5)
+  // Preventing for reloading the form
+  e.preventDefault();
 
-    let arr =[]
+  const data = {
+    title: postTitle.value,
+    body: postBody.value,
+  };
+  // Post request
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json; charset=UTF-8",
+    },
+  };
 
-    top5Posts.forEach(data => {
-        
-        // add to dom function 
-        let newDiv = addToDom(data.title,data.body)
-        arr.push(newDiv)
-        
-    })
+  fetch(url, options)
+    .then((resposne) => resposne.json())
+    .then((post) => {
+      console.log(post);
+      // Adding to DOM
+      // Adding to DOM
+      // Adding to DOM
+      let newDiv = renderPosts(post.title, post.body);
+      blogList.prepend(newDiv);
+    });
 
-    blogList.append(...arr)
+  // Removing the current values
+  postTitle.value = "";
+  postBody.value = "";
+});
 
-})
+// renderPosts function
+function renderPosts(currTitle, currBody) {
+  const title = document.createElement("h3");
+  const p = document.createElement("p");
+  const div = document.createElement("div");
 
- 
-document.getElementById('new-post').addEventListener('submit',function(e){
-    
-    
-    const postTitle = document.getElementById('post-title')
-    const postBody = document.getElementById('post-body')
-    
-    // Preventing for reloading the form 
-    e.preventDefault()
+  title.textContent = currTitle;
+  p.textContent =
+    currBody.substring(0, 1).toUpperCase() + currBody.substring(1);
 
-    const post = {
-        title:postTitle.value,
-        body:postBody.value,
-    }
-        
+  div.append(title, p);
 
-    // Post request 
-    const options = {
-        method:"POST",
-        body :JSON.stringify(post),
-        headers:{
-            "Content-Type":'application/json; charset=UTF-8' 
-        },
-    }
-
-    fetch(url , options)
-        .then(resposne => resposne.json())
-        .then(data => console.log(data))
-     
-
-    
-    // Adding to DOM 
-    let newDiv = addToDom(post.title,post.body)
-    blogList.prepend(newDiv)
-
-    // Removing the current values 
-    postTitle.value = ''
-    postBody.value = ''
-})
-
-
-// addToDom function 
-function addToDom(currTitle,currBody){
-    
-    const  title = document.createElement('h3')
-    const  p = document.createElement('p')
-    const  div = document.createElement('div')
-
-    title.textContent =currTitle
-    p.textContent = currBody.substring(0,1).toUpperCase() + currBody.substring(1,)
-    
-    div.append(title,p)
-
-    return div
+  return div;
 }
-
