7147b7572e466389bb4d853a33d5749952610866
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-03 15:35:04 +0530

    refactor

--- 09.Working with APIs/03. Async JS & War/War Game/index.js
+++ 09.Working with APIs/03. Async JS & War/War Game/index.js
@@ -2,37 +2,35 @@ const newDeckBtn = document.getElementById('new-deck')
 const drawCardBtn = document.getElementById('draw-card')
 const Newurl = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
 
-let deckId ;
+let deckId;
+
+// getting the initial deck of card 
+function getNewDeck() {
+    fetch(Newurl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            deckId = data.deck_id
+            console.log(deckId)
+        })
+}
+
+function drawCardFromDeck() {
+
+    const drawUrl = `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
+
+    console.log(drawUrl)
+    fetch(drawUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+
+}
 
 
 newDeckBtn.addEventListener('click', getNewDeck)
 
-// getting the initial deck of card 
-function getNewDeck(){
-    fetch(Newurl)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-        deckId = data.deck_id
-        console.log(deckId)
-        
-
-    })
-}
-
 // Drawing 2 card from the deck 
 drawCardBtn.addEventListener('click', drawCardFromDeck)
 
-
-function drawCardFromDeck(){
-
-    const drawUrl =  `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
-
-    console.log(drawUrl)
-    fetch(drawUrl)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data)
-    })
-
-}
