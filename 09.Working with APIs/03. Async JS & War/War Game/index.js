b83153b63b3c0ea6e3e76e7522f12417590214e6
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-03 19:59:31 +0530

    application completed

--- 09.Working with APIs/03. Async JS & War/War Game/index.js
+++ 09.Working with APIs/03. Async JS & War/War Game/index.js
@@ -1,27 +1,34 @@
 const Newurl = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
 const newDeckBtn = document.getElementById('new-deck')
 const drawCardBtn = document.getElementById('draw-card')
-const remainingCard = document.getElementById('remaining-card')
-const compImg = document.querySelector('.card-img-comp')
-const userImg = document.querySelector('.card-img-user')
-const compScore = document.getElementById('computer-score')
-const userScore = document.getElementById('user-score')
+let remainingCard = document.getElementById('remaining-card')
+// const compImg = document.querySelector('.card-img-comp')
+// const userImg = document.querySelector('.card-img-user')
+let compScore = document.getElementById('computer-score')
+let userScore = document.getElementById('user-score')
+const cardImg = document.querySelectorAll('.card-img')
+let heading  = document.querySelector('h1')
 
 let deckId;
 
 // getting the initial deck of card 
 function getNewDeck() {
+
+    drawCardBtn.disabled = false
+    compScore.textContent = 0
+    userScore.textContent = 0
+
     fetch(Newurl)
         .then(res => res.json())
         .then(data => {
             console.log(data)
             deckId = data.deck_id
             console.log(deckId)
+            remainingCard.textContent = data.remaining
         })
 }
 
 function drawCardFromDeck() {
-
     const drawUrl = `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
 
     console.log(drawUrl)
@@ -30,26 +37,51 @@ function drawCardFromDeck() {
         .then(data => {
             console.log(data)
 
-            const img1Src = data.cards[0].image
-            const img2Src = data.cards[1].image
+            let imgNumber = 0
+            cardImg.forEach(card => {
+                card.innerHTML = `<img src="${data.cards[imgNumber].image}">`
+                imgNumber+=1
+            })
 
-            const img1 = document.createElement('img')
-            img1.setAttribute('src',img1Src)
-            const img2 = document.createElement('img')
-            img2.setAttribute('src',img2Src)
+            if(data.cards[0].code > data.cards[1].code ){
+                compScore.textContent = parseInt(compScore.textContent) + 1
+            }else{
+                userScore.textContent = parseInt(userScore.textContent) + 1
+            }
+            console.log(compScore,userScore)
+            remainingCard.textContent = parseInt(remainingCard.textContent) - 2
 
-            compImg.innerHTML = ''
-            userImg.innerHTML = ''
-            compImg.append(img1)
-            userImg.append(img2)
-
+            if (remainingCard.textContent == 46){
+                gameEnds()
+            }
         })
 
 }
-
 
 newDeckBtn.addEventListener('click', getNewDeck)
 
 // Drawing 2 card from the deck 
 drawCardBtn.addEventListener('click', drawCardFromDeck)
 
+
+function gameEnds(){
+    drawCardBtn.disabled = true
+
+    console.log('Game ends')
+
+    let userFinalS = parseInt(userScore.textContent)
+    let compFinalS = parseInt(compScore.textContent)
+    
+    console.log(userScore,compScore)
+    console.log(userFinalS,compFinalS)
+    
+    if(compFinalS > userFinalS){
+        heading.textContent = 'Computer Wins'
+    }else if(compFinalS < userFinalS ){
+        heading.textContent = 'User Wins'
+    }else{
+        heading.textContent = 'Draw'
+    }
+
+
+}
