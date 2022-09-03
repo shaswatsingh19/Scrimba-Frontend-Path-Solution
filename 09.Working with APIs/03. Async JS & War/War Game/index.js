5a8c04d736eb48b2f129ec4e064921b3d6f11add
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-03 15:23:08 +0530

    draw a deck of cards

--- 09.Working with APIs/03. Async JS & War/War Game/index.js
+++ 09.Working with APIs/03. Async JS & War/War Game/index.js
@@ -1,1 +1,36 @@
-"https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
+const btn = document.getElementById('btn')
+const Newurl = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
+
+let deckId ;
+
+
+btn.addEventListener('click', getDeck)
+
+// getting the initial deck of card 
+function getDeck(){
+    fetch(Newurl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        deckId = data.deck_id
+        console.log(deckId)
+        
+        // getting two card from deck of card 
+        drawTwoCards(deckId)
+
+    })
+}
+
+
+function drawTwoCards(deckId){
+
+    const drawUrl =  `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
+
+    console.log(drawUrl)
+    fetch(drawUrl)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+
+}
