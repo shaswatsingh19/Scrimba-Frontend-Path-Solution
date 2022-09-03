0752b2772628fc6e29624d1f6550cc6fbcb8bda2
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-03 15:58:19 +0530

    added  css

--- 09.Working with APIs/03. Async JS & War/War Game/index.js
+++ 09.Working with APIs/03. Async JS & War/War Game/index.js
@@ -1,6 +1,7 @@
+const Newurl = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
 const newDeckBtn = document.getElementById('new-deck')
 const drawCardBtn = document.getElementById('draw-card')
-const Newurl = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
+const cards = document.getElementById('cards')
 
 let deckId;
 
@@ -24,6 +25,17 @@ function drawCardFromDeck() {
         .then(res => res.json())
         .then(data => {
             console.log(data)
+
+            const img1Src = data.cards[0].image
+            const img2Src = data.cards[1].image
+
+            const img1 = document.createElement('img')
+            img1.setAttribute('src',img1Src)
+            const img2 = document.createElement('img')
+            img2.setAttribute('src',img2Src)
+
+            cards.innerHTML = ''
+            cards.append(img1,img2)
         })
 
 }
