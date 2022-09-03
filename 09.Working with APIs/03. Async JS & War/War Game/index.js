f31bb2970230f5edc04b915034061bdba41b2a48
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-03 15:28:15 +0530

    refactor code added draw function to draw 2 new card from deck

--- 09.Working with APIs/03. Async JS & War/War Game/index.js
+++ 09.Working with APIs/03. Async JS & War/War Game/index.js
@@ -1,13 +1,14 @@
-const btn = document.getElementById('btn')
+const newDeckBtn = document.getElementById('new-deck')
+const drawCardBtn = document.getElementById('draw-card')
 const Newurl = "https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/"
 
 let deckId ;
 
 
-btn.addEventListener('click', getDeck)
+newDeckBtn.addEventListener('click', getNewDeck)
 
 // getting the initial deck of card 
-function getDeck(){
+function getNewDeck(){
     fetch(Newurl)
     .then(res => res.json())
     .then(data => {
@@ -15,14 +16,15 @@ function getDeck(){
         deckId = data.deck_id
         console.log(deckId)
         
-        // getting two card from deck of card 
-        drawTwoCards(deckId)
 
     })
 }
 
+// Drawing 2 card from the deck 
+drawCardBtn.addEventListener('click', drawCardFromDeck)
 
-function drawTwoCards(deckId){
+
+function drawCardFromDeck(){
 
     const drawUrl =  `https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`
 
