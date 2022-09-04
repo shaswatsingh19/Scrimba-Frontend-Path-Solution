81882a81e42047a5d439ac528940ba45274d0ac6
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-04 21:34:53 +0530

    completed

--- 09.Working with APIs/02. URLs, REST & Blog Space/02 . Color Scheme Generator/index.js
+++ 09.Working with APIs/02. URLs, REST & Blog Space/02 . Color Scheme Generator/index.js
@@ -1,18 +1,86 @@
 const icon = document.querySelector('#icon')
-
-
+const colorPicker = document.getElementById('color-picker')
+const schemeOptions = document.getElementById('scheme')
+const schemeBtn = document.getElementById('color-scheme-btn')
+let colorInHex = '000'
+let selectSchemeValue = 'monochrome'
+const colorFill = document.querySelectorAll('.color-fill')
+const colorText = document.querySelectorAll('.color-text')
+const popUp  = document.getElementById('popup')
 // light and dark mode toggles 
-icon.addEventListener('click',function(e){
 
-    if(document.body.classList.contains('light')){
-        document.body.classList.toggle('light')
-        e.target.src = 'images/sun.png'
-
-        
+window.addEventListener('load',function(){
+    if(this.localStorage.getItem('mode') == 'light'){
+        icon.src = 'images/moon.png'
+        this.document.body.classList.add('light')
     }else{
-        document.body.classList.toggle('light')
-        e.target.src = 'images/moon.png'
-
+        icon.src = 'images/sun.png'
     }
-    
 })
+icon.addEventListener('click', function (e) {
+    
+
+    if (document.body.classList.contains('light')) {
+        e.target.src = 'images/sun.png'
+        localStorage.setItem('mode','dark')
+
+
+    } else {
+        e.target.src = 'images/moon.png'
+        localStorage.setItem('mode','light')
+    }
+    document.body.classList.toggle('light')
+
+
+})
+
+
+colorPicker.addEventListener("input",function(e){
+    colorInHex = e.target.value.substring(1,)
+})
+
+schemeOptions.addEventListener('click',function(e){
+    selectSchemeValue = e.target.value
+})
+
+schemeBtn.addEventListener('click',function(){
+    const urlApi = `https://www.thecolorapi.com/scheme?hex=${colorInHex}&mode=${selectSchemeValue}`
+
+    fetch(urlApi)
+        .then(res => res.json())
+        .then(data => {
+        
+            let i = 0
+            colorText.forEach(text => {
+                text.textContent = data.colors[i].hex.value
+                i+=1
+                text.addEventListener('click',function clickToCopy(){
+                    navigator.clipboard.writeText(text.textContent)
+                    showAndRemovePopUp()
+                })
+            })
+            i = 0
+            colorFill.forEach(color => {
+                color.style.backgroundColor = data.colors[i].hex.value
+                i+=1
+                color.addEventListener('click',function clickToCopy(){
+                    navigator.clipboard.writeText(color.style.backgroundColor)
+                    showAndRemovePopUp()
+                    
+                })
+            })
+        })
+})
+
+
+function showAndRemovePopUp(){
+    setTimeout(() => {
+        
+        popUp.hidden = false
+
+    },300)
+    setTimeout(() => {
+        popUp.hidden = true
+
+    },1200)
+}
