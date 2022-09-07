25b66fb0a2f0f76c812f1890f5c2dd0886099eb6
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-07 19:24:19 +0530

    added time and weather

--- 09.Working with APIs/04. Promise Rejection & Capstone/01.Personal Dashboard/index.js
+++ 09.Working with APIs/04. Promise Rejection & Capstone/01.Personal Dashboard/index.js
@@ -1,18 +1,9 @@
 const cryptoContainer = document.getElementById('crypto-container')
+const weatherContainer = document.getElementById('weather-container')
+const timeContainer  = document.querySelector('#main')
 
-{/* <div id="crypto-container">
-<div class="coin">
-    <div class="coin-header">
-        <img src="" alt="" >
-        <p class="coin-title">Bitcoin</p>
-    </div>
-    <div class="coin-info">
-        <p>🎯: t_price.usd}</p>
-        <p>👆: 4h.usd}</p>
-        <p>👇: h.usd}</p> 
-    </div>
-</div>
-</div> */}
+// ------------------ Unsplash API ----------------------
+
 
 
 // -------------------- Crpyto API --------------------
@@ -44,4 +35,39 @@ fetch(apiCryptoUrl)
 
 // ------------------ Weather API ----------------------
 
-const weatherApiUrl = `  `  
+navigator.geolocation.getCurrentPosition((position) => {
+
+    console.log(position.coords.latitude, position.coords.longitude)
+    const weatherApiUrl = `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`  
+    
+    fetch(weatherApiUrl)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+
+            const imgValue = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+
+
+            weatherContainer.innerHTML = `
+                <img src="${imgValue}" alt="${data.weather[0].main} image">
+                <p class="weather">${data.main.temp}<sup>o</sup></p>
+                <p class="location">${data.name}</p>
+            `
+        })  
+})
+
+
+// ------------------ Time component ----------------------
+
+function getCurrentTime(){
+
+    let currTime = new Date().toLocaleTimeString('en-us' , {timeStyle: 'medium'})
+    timeContainer.textContent = currTime
+
+}
+setInterval(getCurrentTime , 1000)
+
+
+
+
+
