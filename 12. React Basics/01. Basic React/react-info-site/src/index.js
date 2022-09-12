5f5048c5166375c55c7436cbe3d05cbd395b05c4
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-12 15:49:00 +0530

    added css

--- 12. React Basics/01. Basic React/react-info-site/src/index.js
+++ 12. React Basics/01. Basic React/react-info-site/src/index.js
@@ -1,48 +1,51 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom/client';
-import './App.css';
-import logoImg from './logo.svg'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./App.css";
+import logoImg from "./logo.svg";
 
 // Components
-function Header (){
+function Header() {
   return (
     <nav>
-      <img src={logoImg} alt='react logo'></img>
+      <img className='logo' src={logoImg} alt="react logo"></img>
+      <ul className="nav-list">
+        <li>Pricing</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-function Main(){
+function Main() {
   return (
     <main>
       <h1>Fun facts about React</h1>
-      <ul>
+      <ol>
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 100k stars on Github</li>
-        <li>Is maintained by Facebook</li>  
+        <li>Is maintained by Facebook</li>
         <li>Powers thousand of enterprise apps, including mobile apps</li>
-      </ul>
+      </ol>
     </main>
-  )
+  );
 }
 
-function Footer(){
+function Footer() {
   return (
     <footer>
       <small>© 2022 Singh development. All rights reserved</small>
     </footer>
-  )
+  );
 }
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-    <div id ='container'>
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-)
+  <div id="container">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+);
