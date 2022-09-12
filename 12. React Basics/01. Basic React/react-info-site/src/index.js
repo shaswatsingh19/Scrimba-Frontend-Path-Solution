48211ff9551e1fcaeb5ce1545b537ba089ec7fda
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-12 15:17:56 +0530

    created different components

--- 12. React Basics/01. Basic React/react-info-site/src/index.js
+++ 12. React Basics/01. Basic React/react-info-site/src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css';
+import logoImg from './logo.svg'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Components
+function Header (){
+  return (
+    <nav>
+      <img src={logoImg} alt='react logo'></img>
+    </nav>
+  )
+}
+
+function Main(){
+  return (
+    <main>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>  
+        <li>Powers thousand of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  )
+}
+
+function Footer(){
+  return (
+    <footer>
+      <small>© 2022 Singh development. All rights reserved</small>
+    </footer>
+  )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <div id ='container'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+)
