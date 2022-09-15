cc304e927d2d11fa68e945cd5394408e8909a161
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-15 22:15:23 +0530

    refactoring

--- 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/components/Hero.jsx
+++ 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/components/Hero.jsx
@@ -4,7 +4,6 @@ export default function Hero(){
     return (
         <div className='hero'>
             <img id='main-img' src={photoGrid} alt='main page grid image'></img>
-
             <h1>Online Experiences</h1>
             <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
         </div>
--- 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/components/Navbar.jsx
+++ 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import airbnbLogo from '../assets/airbnb-logo.png'
-import photoGrid from '../assets/photo-grid.png'
 
 function Navbar(){
     return (
         <nav>
             
             <img id='nav-logo' src={airbnbLogo} alt='airbnb logo'></img>
+
+
         
         </nav>
     )
