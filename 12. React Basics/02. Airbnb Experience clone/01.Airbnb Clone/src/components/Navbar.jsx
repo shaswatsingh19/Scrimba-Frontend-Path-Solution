0e989a83c1917c868bba3d66436ee1df59c329b9
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-15 21:50:19 +0530

    navbar component

--- 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/components/Navbar.jsx
+++ 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/components/Navbar.jsx
@@ -1,12 +1,18 @@
-
+import airbnbLogo from '../assets/airbnb-logo.png'
+import photoGrid from '../assets/photo-grid.png'
 
 function Navbar(){
     return (
         <nav>
             <header>
-                
+                <img id='nav-logo' src={airbnbLogo} alt='airbnb logo'></img>
             </header>
-            <main></main>
+            <div className='hero'>
+                <img id='main-img' src={photoGrid} alt='main page grid image'></img>
+
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            </div>
         </nav>
     )
 }
