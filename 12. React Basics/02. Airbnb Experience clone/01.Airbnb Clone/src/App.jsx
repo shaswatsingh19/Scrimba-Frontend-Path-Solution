370a57405282d85dbbf25c1ea58ae8860412c098
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-17 21:48:27 +0530

    used props

--- 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/App.jsx
+++ 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/App.jsx
@@ -3,13 +3,21 @@ import Navbar from './components/navbar'
 import Hero from './components/hero'
 import Card from './components/card'
 
+
 function App() {
   
   return (
     <div className="App">
       <Navbar />
       <Hero />
-      <Card />
+      <Card 
+          img='katie-zaferes.png'
+          rating={5.0}
+          reviewCount={6}
+          country='USA'
+          title='Life lesson with Katie Zaferes'
+          price={136}
+      />
     </div>
   )
 }
--- 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/components/Card.jsx
+++ 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/components/Card.jsx
@@ -1,24 +1,23 @@
-import KatieImg from '../assets/katie-zaferes.png'
 import starPng from '../assets/star.png'
 
  
-export default function Card(){
+export default function Card(props){
 
     return (
         <div className='card'>
             <div className="card-img">
-                <img src={KatieImg} alt='user images'></img>
+                <img src={`src/assets/${props.img}`} alt='user images'></img>
                 <p className="availablity">Sold Out</p>
             </div>
         
             <div className='rating'>
                 <img src={starPng} alt='star icon'></img>
-                <span className='current-rating'>5.0</span>  
-                <span className='rated-by'>(6) • </span>  
-                <span className='country'>USA</span>  
+                <span className='current-rating'>{props.rating}</span>  
+                <span className='rated-by'>({props.reviewCount}) • </span>  
+                <span className='country'>{props.country}</span>  
             </div>
-            <p className='title'>Life lesson with Katie Zaferes</p>
-            <p><strong>From</strong> <span class='price'>$136</span> / person</p> 
+            <p className='title'>{props.title}</p>
+            <p><strong>From</strong> <span className='price'>${props.price}</span> / person</p> 
         </div>
     )
 } 
