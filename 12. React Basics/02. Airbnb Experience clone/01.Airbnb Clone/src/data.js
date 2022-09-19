e986f2203eb1a59cf79d8ecd932b11a2f511a43b
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-19 20:53:09 +0530

    refactor

--- 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/data.js
+++ 12. React Basics/02. Airbnb Experience clone/01.Airbnb Clone/src/data.js
@@ -1,4 +1,4 @@
-import mount from '../public/mountain-bike.png'
+
 
 export default [
     {
@@ -6,7 +6,7 @@ export default [
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: "https://i.pinimg.com/736x/47/2b/61/472b61d49bb2c847bb7e0665497af47e.jpg",
+        coverImg: "..public/katie-zaferes.png",
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -19,7 +19,7 @@ export default [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: "../public/assets/wedding-photography.png",
+        coverImg: "../public/wedding-photography.png",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -32,7 +32,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: mount,
+        coverImg: '../public/mountain-bike.png',
         stats: {
             rating: 4.8,
             reviewCount: 2
