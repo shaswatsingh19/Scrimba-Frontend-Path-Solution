9c4d75d8889c42dfd6866b018f9836819a0d2b18
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-21 20:56:56 +0530

    updated the footer link

---
+++ 12. React Basics/01. Basic React/Digital Business Card/digital-business-card/src/data.js
@@ -0,0 +1,11 @@
+export const Data = [
+    {
+        id:"about",
+        content:"I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn."
+    },
+    {
+        id:"interests",
+        content:"Bodyweight workout. Gamer. Reader. Internet fanatic. Travel geek. Pop culture ninja. Tea fanatic. Food Lover."
+
+    }
+]
