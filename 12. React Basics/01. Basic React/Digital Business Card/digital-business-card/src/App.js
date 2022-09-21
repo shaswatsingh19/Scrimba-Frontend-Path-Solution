abd78a055713b46291e126af279b3627fa0b5073
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-21 10:48:11 +0530

    completed layout will do css now

--- 12. React Basics/01. Basic React/Digital Business Card/digital-business-card/src/App.js
+++ 12. React Basics/01. Basic React/Digital Business Card/digital-business-card/src/App.js
@@ -2,11 +2,11 @@ import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
 
-
 export default function App(){
     return (
         <div id='container'>
             <Header/>
+            
             <Main />
             <Footer />
         </div>
--- 12. React Basics/01. Basic React/Digital Business Card/digital-business-card/src/components/Main.js
+++ 12. React Basics/01. Basic React/Digital Business Card/digital-business-card/src/components/Main.js
@@ -1,5 +1,15 @@
 export default function Header(){
     return (
-        <h1>Main</h1>
+        <main>
+            <div id='about'>
+                <h3>About</h3>
+                <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+            </div>
+            
+            <div id='interests'>
+                <h3>Interests</h3>
+                <p>Bodyweight workout. Gamer. Reader. Internet fanatic. Travel geek. Pop culture ninja. Tea fanatic. Food Lover</p>
+            </div>
+        </main>
     )
 }
