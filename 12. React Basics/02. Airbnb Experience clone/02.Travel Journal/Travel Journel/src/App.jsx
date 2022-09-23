0937553ca120f480b8917f077bcfaf195160d8fc
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-23 09:25:43 +0530

    created component for main section

--- 12. React Basics/02. Airbnb Experience clone/02.Travel Journal/Travel Journel/src/App.jsx
+++ 12. React Basics/02. Airbnb Experience clone/02.Travel Journal/Travel Journel/src/App.jsx
@@ -1,11 +1,23 @@
 import Navbar from './components/Navbar'
 import Main from './components/Main'
-
+import Data from './data'
 function App() {
+
+  
+  const main = Data.map(data => {
+    return (
+      <Main 
+        key = {data.id}
+        item = {data}
+      
+      />
+    )
+  })
+
   return (
     <div id='container'>
       <Navbar />
-      <Main />
+      {main}
     </div>
   )
 }
--- 12. React Basics/02. Airbnb Experience clone/02.Travel Journal/Travel Journel/src/components/Main.jsx
+++ 12. React Basics/02. Airbnb Experience clone/02.Travel Journal/Travel Journel/src/components/Main.jsx
@@ -1,20 +1,20 @@
-export default function Main(){
+export default function Main(props){
     return (
         <main>
             <section className="img-section">
-                <img src="https://source.unsplash.com/WLxQvbMyfas" alt=""></img>
+                <img src={props.item.image} alt={props.item.title}></img>
             </section>
             <section className="details">
                 <div className="location">
                     <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" clip-rule="evenodd" d="M3.57866 5.25002C2.65532 5.25002 1.90837 4.50221 1.90837 3.57965C1.90837 2.65757 2.65532 1.90911 3.57866 1.90911C4.50167 1.90911 5.24927 2.65757 5.24927 3.57965C5.24927 4.50221 4.50167 5.25002 3.57866 5.25002ZM3.48394 0C1.54227 0 0 1.63678 0 3.65569C0 6.40791 2.95078 9.25191 2.95078 9.25191C3.34777 9.62783 3.59437 9.65847 4.01646 9.25191C4.01646 9.25191 7 6.40791 7 3.65569C7 1.63678 5.42544 0 3.48394 0Z" fill="#F55A5A"/>
                     </svg>
-                    <p className="country">JAPAN</p>
-                    <a href='#' target={'_blank'}>View on Google Maps</a>
+                    <p className="country">{props.item.location}</p>
+                    <a href={props.item.googleMapsUrl} target={'_blank'}>View on Google Maps</a>
                 </div>
-                <h1>Mount Fuji</h1>
-                <h4><span>12 Jan</span> - <span>24 Jan</span></h4>
-                <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <h1>{props.item.title}</h1>
+                <h4><span>{props.item.startDate}</span> - <span>{props.item.endDate}</span></h4>
+                <p>{props.item.description}</p>
             </section>
         </main>
     )
