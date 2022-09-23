860196ef8e4a4757862ec69dd585dc7eb5d5df8e
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-23 10:02:25 +0530

    added styling

--- 12. React Basics/02. Airbnb Experience clone/02.Travel Journal/Travel Journel/src/data.jsx
+++ 12. React Basics/02. Airbnb Experience clone/02.Travel Journal/Travel Journel/src/data.jsx
@@ -8,5 +8,25 @@ export default [
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
         image: "mount_fuji.jpg"
+    },
+    {
+        id: 2,
+        title: "Opera House",
+        location: "Australia",
+        googleMapsUrl: "https://goo.gl/maps/kScCBdBcpGj3fV1c8",
+        startDate: "21 Mar, 2021",
+        endDate: "24 Mar, 2021",
+        description: "The Geirangerfjord is one of Norway's most visited tourist sites and has been included on the UNESCO World Heritage List, along with the Nærøyfjord, since 2005.",
+        image: "opera_house.jpg"
+    },
+    {
+        id: 3,
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsUrl: "https://goo.gl/maps/T236AVp5RqS7sH136",
+        startDate: "9 Jul, 2021",
+        endDate: "13 Jul, 2021",
+        description: "The Geirangerfjord is one of Norway's most visited tourist sites and has been included on the UNESCO World Heritage List, along with the Nærøyfjord, since 2005.",
+        image: "geirangerfjord.jpg"
     }
 ]
