87348f5e6e2db0b904c81349f0072dfc821fe119
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-24 17:12:39 +0530

    refactor code

--- 12. React Basics/03. Meme Generator/Meme Generator/src/components/Header.jsx
+++ 12. React Basics/03. Meme Generator/Meme Generator/src/components/Header.jsx
@@ -1,15 +1,16 @@
 
 
 export default function Header(){
+
+    function changeTheme(){
+        document.body.classList.toggle('theme')                    
+    }
+
     return (
         <header>
             <img src='Troll Face.png' alt='page log' ></img>
             <h1>Meme Generator</h1>
-            <button className='icon' onClick={
-                function (){
-                    document.body.classList.toggle('theme')                    
-                }
-            }>Theme</button>
+            <button className='icon' onClick={changeTheme}>Theme</button>
             </header>
     )
 }
