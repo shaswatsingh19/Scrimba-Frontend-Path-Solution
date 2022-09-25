6bffaea93774a3d6669c42b823d8490f9f8df92c
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-25 14:59:22 +0530

    apply useState to header component if changing theme

--- 12. React Basics/03. Meme Generator/Meme Generator/src/components/Header.jsx
+++ 12. React Basics/03. Meme Generator/Meme Generator/src/components/Header.jsx
@@ -1,16 +1,24 @@
-
+import React from 'react'
 
 export default function Header(){
 
+    const [theme , setTheme ] = React.useState('Dark')
+
     function changeTheme(){
-        document.body.classList.toggle('theme')                    
+        
+        document.body.classList.toggle('theme')   
+        setTheme((prevTheme) => {
+            if (prevTheme == 'Dark' ){
+                return 'Light'
+            }return 'Dark'
+        })              
     }
 
     return (
         <header>
             <img src='Troll Face.png' alt='page log' ></img>
             <h1>Meme Generator</h1>
-            <button className='icon' onClick={changeTheme}>Theme</button>
-            </header>
+            <button className='icon' onClick={changeTheme}>{theme}</button>
+        </header>
     )
 }
