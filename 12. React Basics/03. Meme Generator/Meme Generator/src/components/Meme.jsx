bf04eaa73506d9e37304a42c111ef463808a2052
shaswat singh <shaswatsingh.ose@gmail.com>
2022-09-25 20:39:59 +0530

    added random image and learned about useState hook to change it

--- 12. React Basics/03. Meme Generator/Meme Generator/src/components/Meme.jsx
+++ 12. React Basics/03. Meme Generator/Meme Generator/src/components/Meme.jsx
@@ -1,11 +1,43 @@
+import React from 'react'
+import memesData from '../memesData'
+
 export default function Meme(){
+
+    // Initial state for Meme image
+    const [ meme , setMeme ]  = React.useState({
+        topText : "",
+        bottomText : "",
+        randomImage : 'https://i.imgflip.com/22bdq6.jpg'
+    })
+
+    const [allMemeData , setAllMemeData ] = React.useState(memesData)
+
+
+
+    function getMemeImage(){
+
+        const memesArr = allMemeData.data.memes
+        const randomInd = Math.floor(Math.random() * memesArr.length)
+        const url = memesArr[randomInd].url
+        
+        setMeme((prevState) => {
+            return {
+                ...prevState,
+                randomImage : url
+            }
+        })
+    }
+
     return (
         <main>
-            <form>
-                <input type='text' id='top-text' name='top-text'></input>
-                <input type='text' id='bottom-text' name='bottom-text'></input>
-                <button type="submit">Get a new meme image</button>
-            </form>
+            <div className="form">
+            
+                <input type='text' id='top-text' placeholder='top text' name='top-text'></input>
+                <input type='text' id='bottom-text' placeholder='bottom text' name='bottom-text'></input>
+                <button type="submit" onClick = {getMemeImage}>Get a new meme image 🖼</button>
+
+                <img src={meme.randomImage}alt = 'meme image'></img>
+            </div>
         </main>
     )
 }
