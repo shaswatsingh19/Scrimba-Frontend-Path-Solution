125e194061373774d7f772a940075bb2f0e2f463
shaswat singh <shaswatsingh.ose@gmail.com>
2022-10-08 20:11:14 +0530

    refactor header light/dark theme

--- 12. React Basics/03. Meme Generator/Meme Generator/src/components/Header.jsx
+++ 12. React Basics/03. Meme Generator/Meme Generator/src/components/Header.jsx
@@ -5,23 +5,18 @@ import logo  from '../assets/TrollFace.png'
 
 export default function Header(){
 
-    const [theme , setTheme ] = React.useState('Dark')
+    const [theme , setTheme ] = React.useState(true)
 
     function changeTheme(){
-        
         document.body.classList.toggle('theme')   
-        setTheme((prevTheme) => {
-            if (prevTheme == 'Dark' ){
-                return 'Light'
-            }return 'Dark'
-        })              
+        setTheme(!theme)              
     }
 
     return (
         <header>
             <img src={logo} alt='page logo' ></img>
             <h1>Meme Generator</h1>
-            <button className='icon' onClick={changeTheme}>{theme}</button>
+            <button className='icon' onClick={changeTheme}>{theme ? 'Dark' : "Light"}</button>
         </header>
     )
 }
