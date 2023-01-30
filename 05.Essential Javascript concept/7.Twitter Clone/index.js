20c72bdccaf3e9e08df22b460c202026d6bbd27c
shaswat singh <shaswatsingh.ose@gmail.com>
2023-01-30 09:12:56 +0530

    removed the comment

--- 05.Essential Javascript concept/7.Twitter Clone/index.js
+++ 05.Essential Javascript concept/7.Twitter Clone/index.js
@@ -1,12 +1,3 @@
-/*
-Challenge:
-1. Put all of the data in its own file called
-   data.js, and export it back into index.js. 
-   Make any changes to index.html that are
-   necessary to make this work.
-2. Log out tweetsData.
-*/
-
 import {tweetsData} from './data.js'
 
 console.log(tweetsData)
