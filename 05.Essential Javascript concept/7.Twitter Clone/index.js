11b68d85f5e899b9c9392f37d5dc6cfdad143802
shaswat singh <shaswatsingh.ose@gmail.com>
2023-01-31 10:57:18 +0530

    added icons and like functionality

--- 05.Essential Javascript concept/7.Twitter Clone/index.js
+++ 05.Essential Javascript concept/7.Twitter Clone/index.js
@@ -11,6 +11,33 @@ tweetBtn.addEventListener('click',() => {
     }
 })
 
+document.addEventListener('click',(e) => {
+    if(e.target.dataset.like){
+        handleLikeClicked(e.target.dataset.like)
+    }else if(e.target.dataset.reply){
+        console.log(e.target.dataset)
+    }else if(e.target.dataset.retweet){
+        console.log(e.target.dataset)
+    }
+})
+
+function handleLikeClicked(tweetId){
+
+    console.log('liked ',tweetId)
+    const targetTweetObj = tweetsData.filter(data => {
+        if(data.uuid === tweetId){
+            return data
+        }
+    })[0]
+    if(targetTweetObj.isLiked){
+        targetTweetObj.likes-=1
+        targetTweetObj.isLiked = false
+    }else{
+        targetTweetObj.likes+=1
+        targetTweetObj.isLiked = true
+    }
+    renderFeed() // To render new like count UI
+}
 
 function getFeedHtml(){
     const tweetArr = []
@@ -24,12 +51,15 @@ function getFeedHtml(){
                     <p class="tweet-text">${data.tweetText}</p>
                     <div class="tweet-details">
                         <span class="tweet-detail">
+                            <i class="fa-regular fa-comment-dots" data-reply=${data.uuid}></i>
                             ${data.replies.length}
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid fa-heart" data-like=${data.uuid}></i>
                             ${data.likes}
                         </span>
                         <span class="tweet-detail">
+                            <i class="fa-solid fa-retweet" data-retweet=${data.uuid}></i>
                             ${data.retweets}
                         </span>
                     </div>   
